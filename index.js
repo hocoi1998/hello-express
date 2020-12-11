6f45c97497d21381e9ad56e87b57bdfe20f12001
Dinh Toan <toank61humg@gmail.com>
2020-12-11 22:22:18 +0700

    to be continue...

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
+
+var userRoute = require('./routes/user.route')
 
 var port = 3000;
 
-app.get('/', function(request, response) {
-  response.send("<h1>Hello Coders-X</h1>");
+var app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.get('/', function(req, res) {
+  res.render("index", {
+    'name': 'AAA'
+  });
 });
+
+app.use('/users', userRoute);
 
 app.listen(port, function() {
   console.log('Server listening on port ' + port);
